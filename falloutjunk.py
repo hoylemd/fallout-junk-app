4d5720752573f1d37550eeb6747b6d4b3fc83881
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-02 11:51:55 -0500

    add some methods for requests

--- falloutjunk.py
+++ falloutjunk.py
@@ -1,6 +1,6 @@
 import sqlite3
-from flask import Flask, request, session, g, redirect, url_for, \
-    abort, render_template, flash
+from flask import Flask, g, render_template  # , request, session, redirect, \
+#    url_for, abort, flash
 from contextlib import closing
 
 # configuration
@@ -11,17 +11,42 @@ USERNAME = 'admin'
 PASSWORD = 'buttslol'
 
 
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+
 def init_db():
     with closing(connect_db()) as db:
         with app.open_resource('schema.sql', mode='r') as f:
             db.cursor().executescript(f.read())
         db.commit()
 
-def connect_db():
-    return sqlite3.connect(app.config['DATABASE'])
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+
+@app.teardown_request
+def after_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
+
+@app.route('/')
+def show_components():
+    cursor = g.db.execute('select * from components order by id desc;')
+    entries = []
+    for row in cursor.fetchal():
+        entry = {
+            'slug': row[0], 'name': row[1], 'value': row[2], 'weight': row[3]}
+        entries.append(entry)
+    return render_template('show_components.html', entries=entries)
 
 if __name__ == '__main__':
     app.run()
