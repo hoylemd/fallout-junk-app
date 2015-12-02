4fec056eea08293d6bf4baabb5b922cb8c1138b6
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-02 12:09:26 -0500

    add login and logout routes

--- falloutjunk.py
+++ falloutjunk.py
@@ -67,5 +67,28 @@ def add_component():
     flash('Component was successfully created')
     return redirect(url_for('show_components'))
 
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Incorrect username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Incorrect password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_components'))
+
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_components'))
+
 if __name__ == '__main__':
     app.run()
