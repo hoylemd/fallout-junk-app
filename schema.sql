37359e0118d795c3648eb1a628157202f2e7b03d
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-02 12:24:21 -0500

    add the login and content templates

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 drop table if exists components;
 create table components (
   id integer primary key autoincrement,
-  slug varchar[32] not null,
+  slug varchar[64] not null,
   name varchar[64] not null,
   int value default 0,
   decimal weight default 0
