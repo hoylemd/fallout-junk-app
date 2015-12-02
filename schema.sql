ac7688e2df8cb25b64d68d82c30f0d93ea906fbd
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-02 13:20:34 -0500

    add some constraints to the schema

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 drop table if exists components;
 create table components (
   id integer primary key autoincrement,
-  slug varchar[64] not null,
+  slug varchar[64] not null unique,
   name varchar[64] not null,
   value int default 0,
   weight decimal default 0
@@ -10,20 +10,22 @@ create table components (
 drop table if exists junk;
 create table junk (
   id integer primary key autoincrement,
-  slug varchar[64] not null,
+  slug varchar[64] not null unique,
   name varchar[64] not null,
   value int default 0,
   weight decimal default 0,
+  components_value int,
+  components_weight decimal,
   craftable integer default 0,
   used_for_crafting integer default 0
 );
 
 drop table if exists junk_compositions;
 create table junk_compositions (
-  id integer primary key autoincrement,
   junk_id integer not null,
   component_id integer not null,
   count integer default 1,
   foreign key(junk_id) references junk(id),
   foreign key(component_id) references components(id)
+  primary key(junk_id, component_id)
 )
