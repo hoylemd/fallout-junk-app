c748ca6cef206b2b63a25480780623a1d5a74440
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-03 11:53:54 -0500

    make the test work

--- falloutjunk_test.py
+++ falloutjunk_test.py
@@ -3,22 +3,23 @@ import falloutjunk
 from nose import with_setup
 import tempfile
 
-test_app = None
+context = type('app_context', (object,), {'db_fd': None, 'app': None})
 
 
 def set_up():
-    test_app.db_fd, falloutjunk.app.config['DATABASE'] = tempfile.mkstemp()
+    context.db_fd, falloutjunk.app.config['DATABASE'] = tempfile.mkstemp()
     falloutjunk.app.config['TESTING'] = True
-    test_app.app = falloutjunk.app.test_client()
+    context.app = falloutjunk.app.test_client()
     falloutjunk.init_db()
 
 
 def tear_down():
-    os.close(test_app.db_fd)
+    os.close(context.db_fd)
     os.unlink(falloutjunk.app.config['DATABASE'])
 
 
 @with_setup(set_up, tear_down)
-def test_root__empty():
-    rv = test_app.get('/')
-    assert 'No entries here so far' in rv.data
+def test_root__redirects_to_junk():
+    rv = context.app.get('/')
+    assert rv.status == '302 FOUND'
+    assert 'href="/junk"' in rv.data
