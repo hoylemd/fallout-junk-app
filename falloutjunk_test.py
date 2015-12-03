4444280ac367f1995bf4099e24fa02de0f15c42c
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-03 13:40:45 -0500

    add logged in and not logged in tests for add_junk

--- falloutjunk_test.py
+++ falloutjunk_test.py
@@ -4,6 +4,10 @@ import unittest
 import tempfile
 
 
+def get_redirected_path(location):
+    return location[location.rfind('/'):]
+
+
 class FalloutJunkTestCase(unittest.TestCase):
 
     def setUp(self):
@@ -16,11 +20,48 @@ class FalloutJunkTestCase(unittest.TestCase):
         os.close(self.db_fd)
         os.unlink(falloutjunk.app.config['DATABASE'])
 
+    def login(self, username, password):
+        return self.app.post('/login', data=dict(
+            username=username,
+            password=password
+        ), follow_redirects=True)
+
+    def logout(self):
+        return self.app.get('/logout', follow_redirects=True)
+
     def test_root__redirects_to_junk(self):
         rv = self.app.get('/')
         assert rv.status == '302 FOUND'
         assert 'href="/junk"' in rv.data
 
-    def test_junk__200_OK(self):
+    def test_get_junk__200_OK(self):
         rv = self.app.get('/junk')
         assert rv.status == '200 OK'
+
+    def test_post_add_junk__401_not_authenticated(self):
+        payload = {
+            'name': '10lb Weight',
+            'value': 2,
+            'weight': 10,
+            'components_value': 3,
+            'components_weight': 0.9,
+            'craftable': 'on',
+            'used_for_crafting': 'off'
+        }
+        rv = self.app.post('/add_junk', data=payload)
+        assert rv.status == '401 UNAUTHORIZED'
+
+    def test_post_add_junk__302_all_fields(self):
+        payload = {
+            'name': '10lb Weight',
+            'value': 2,
+            'weight': 10,
+            'components_value': 3,
+            'components_weight': 0.9,
+            'craftable': 'on',
+            'used_for_crafting': 'off'
+        }
+        self.login('admin', 'buttslol')
+        rv = self.app.post('/add_junk', data=payload)
+        assert rv.status == '302 FOUND'
+        assert get_redirected_path(rv.headers['location']) == '/junk'
